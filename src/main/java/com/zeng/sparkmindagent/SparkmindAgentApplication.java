fe593d0655467955abea15aa8246981e89082721
Eii3 <jeungbernice@outlook.com>
2026-03-28 20:55:33 +0800

    init commit

---
+++ src/main/java/com/zeng/sparkmindagent/SparkmindAgentApplication.java
@@ -0,0 +1,13 @@
+package com.zeng.sparkmindagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkmindAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SparkmindAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zeng/sparkmindagent/SparkmindAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeng.sparkmindagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkmindAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
