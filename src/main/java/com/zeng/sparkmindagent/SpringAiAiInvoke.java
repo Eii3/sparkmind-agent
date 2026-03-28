45f569490ae3a179a5b0f6894d5e1439efacad53
Eii3 <jeungbernice@outlook.com>
2026-03-28 21:54:21 +0800

    Spring AI接入大模型

---
+++ src/main/java/com/zeng/sparkmindagent/SpringAiAiInvoke.java
@@ -0,0 +1,23 @@
+package com.zeng.sparkmindagent;
+
+import jakarta.annotation.Resource;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringAiAiInvoke implements CommandLineRunner {
+
+    @Resource
+    private ChatModel dashscopeChatModel;
+
+    @Override
+    public void run(String... args) throws Exception {
+        AssistantMessage output = dashscopeChatModel.call(new Prompt("hello i am zeng."))
+                .getResult()
+                .getOutput();
+        System.out.println(output.getText());
+    }
+}
